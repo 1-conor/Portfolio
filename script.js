68b2cc8fdda30d8142a6bb57a0c32022c1aae89f
Conor Houston <houston.conor@outlook.com>
2024-05-15 15:19:14 +0100

    Somewhat finished layout & design to be filled in

--- script.js
+++ script.js
@@ -9,3 +9,49 @@ const calculateTimeSince = dateString => {
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('years').textContent = calculateTimeSince("2023-09-18");
 });
+
+
+var modal1 = document.getElementById("modal-1");
+var btn1 = document.getElementById("project-1");
+var span1 = modal1.getElementsByClassName("close")[0];
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+
+var modal2 = document.getElementById("modal-2");
+var btn2 = document.getElementById("project-2");
+var span2 = modal2.getElementsByClassName("close")[0];
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+var modal3 = document.getElementById("modal-3");
+var btn3 = document.getElementById("project-3");
+var span3 = modal3.getElementsByClassName("close")[0];
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+}
